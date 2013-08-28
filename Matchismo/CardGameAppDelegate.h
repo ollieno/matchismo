db6ecf11b78b89d6f18a8eb7d5daf44329f98859
Jeroen Olthof <jeroen@thewebmen.com>
2013-08-28 22:33:33 +0200

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Jeroen Olthof on 28-08-13.
+//  Copyright (c) 2013 Jeroen Olthof. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Jeroen Olthof on 28-08-13.
+//  Copyright (c) 2013 Jeroen Olthof. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
